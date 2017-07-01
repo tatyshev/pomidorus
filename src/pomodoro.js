8e227c85df87ea2e2eb7b96420856cff48377b06
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2017-07-01 19:30:10 +0600

    pomodoro optional timeout

--- src/pomodoro.js
+++ src/pomodoro.js
@@ -1,12 +1,13 @@
 export const DEFAULT_TYPE = 'default';
 export const DEFAULT_DURATION = 25 * 60;
-export const DEFAULT_TICK = 1000;
+export const DEFAULT_TIMEOUT = 1000;
 
 export default class Pomodoro {
   constructor (options = {}) {
     this.type = options.type || DEFAULT_TYPE;
     this.duration = options.duration || DEFAULT_DURATION;
     this.started = true;
+    this.timeout = options.timeout || DEFAULT_TIMEOUT;
     this.timer = null;
   }
 
@@ -14,7 +15,7 @@ export default class Pomodoro {
     if (this.started) { return }
 
     let tick = this.tick.bind(this);
-    this.timer = setTimeout(tick, DEFAULT_TICK);
+    this.timer = setTimeout(tick, this.timeout);
   }
 
   tick () {

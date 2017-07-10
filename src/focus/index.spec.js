397416d5ef4ab578eb1b2070721236d6e8dfba94
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2017-07-10 22:21:08 +0600

    add spec when at least one pomodoro are finished

--- src/focus/index.spec.js
+++ src/focus/index.spec.js
@@ -1,4 +1,4 @@
-import Pomodoro, { DEFAULT_INTERVAL } from './pomodoro';
+import Pomodoro, { DEFAULT_INTERVAL, DEFAULT_DURATION } from './pomodoro';
 import Focus from './index';
 
 jest.useFakeTimers();
@@ -15,6 +15,20 @@ describe('Focus.rotate()', () => {
     it('should push pomodoro to item', () => {
       focus.rotate();
       expect(focus.latest).toBeInstanceOf(Pomodoro);
+    });
+  });
+
+  describe('when pomodoro are finished', () => {
+    it('should call rotate() when pomodoro are finished', () => {
+      focus.rotate();
+      const latest = focus.latest;
+
+      latest.state = DEFAULT_DURATION;
+      focus.rotate = jest.fn();
+
+      tick();
+
+      expect(focus.rotate).toBeCalled();
     });
   });
 });

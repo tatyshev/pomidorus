3c5f2bb8bead059fcacb8004b24cf32868f261e4
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2017-07-17 15:16:24 +0600

    minor fixes

--- src/components/Root.spec.js
+++ src/components/Root.spec.js
@@ -11,20 +11,18 @@ beforeEach(() => {
   button = root.vm.$refs.button;
 });
 
-describe('initial', () => {
-  it('should set initial values', () => {
-    expect(state).toBeInstanceOf(HTMLElement);
-    expect(button).toBeInstanceOf(HTMLElement);
-    expect(state.innerHTML).toBe('0');
-  });
+it('should set initial values', () => {
+  expect(state).toBeInstanceOf(HTMLElement);
+  expect(button).toBeInstanceOf(HTMLElement);
+  expect(state.innerHTML).toBe('0');
+});
 
-  it('should increment state when button clicked', (done) => {
-    button.click();
-    button.click();
+it('should increment state when button clicked', (done) => {
+  button.click();
+  button.click();
 
-    root.vm.$nextTick(() => {
-      expect(state.innerHTML).toBe('2');
-      done();
-    });
+  root.vm.$nextTick(() => {
+    expect(state.innerHTML).toBe('2');
+    done();
   });
 });

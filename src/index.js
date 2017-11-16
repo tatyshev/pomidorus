4cb36516f4681604a69a9f40daeb220b454b407a
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2017-11-16 23:55:14 +0300

    no vuex

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import Vue from 'vue';
 import Application from '@/components/Application';
-import store from '@/store';
 
 Vue.config.productionTip = false;
+
 Vue.filter('zeroify', x => `0${x}`.slice(-2));
 
 export default new Vue({
-  store,
   el: '#app',
   render: h => h(Application),
 });
--- src/store/const.js
+++
@@ -1,3 +0,0 @@
-export const DEFAULT_TYPE = 'DEFAULT_TYPE';
-export const SHORT_TYPE = 'SHORT_TYPE';
-export const LONG_TYPE = 'LONG_TYPE';

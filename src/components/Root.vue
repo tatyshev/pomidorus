e519665f72a86134b34f82736e9a4fec7cca6401
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2017-12-02 11:16:54 +0300

    WIP

--- src/components/Root.vue
+++ src/components/Root.vue
@@ -1,23 +1,59 @@
+<style src="@/styles/index.scss" lang="scss">
+</style>
+
 <template>
   <div class="b-root">
-    <div class="b-root__body">
-      <ul class="b-root__tabs">
-        <li>One</li>
-        <li>Two</li>
-        <li>Three</li>
-      </ul>
 
-      <slick class="b-root__items">
-        <div class="b-root__item">One</div>
-        <div class="b-root__item">Two</div>
-        <div class="b-root__item">Three</div>
-      </slick>
+    <div class="slider js_slider b-root__sections" ref="sections">
+      <div class="frame js_frame">
+        <ul class="slides js_slides">
+          <li class="js_slide b-root__section">One</li>
+          <li class="js_slide b-root__section">Two</li>
+          <li class="js_slide b-root__section">Three</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+  import { lory } from 'lory.js';
+
   export default {
     name: 'root',
+
+    mounted() {
+      lory(this.$refs.sections);
+    },
   };
 </script>
+
+<style lang="scss">
+  .slider {
+    .frame {
+      width: 100%;
+      position: relative;
+      font-size: 0;
+      line-height: 0;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+
+    .slides {
+      width: 100%;
+      display: inline-block;
+    }
+
+    .js_slide {
+      position: relative;
+      display: inline-block;
+      width: 100%;
+      position: relative;
+      display: inline-block;
+      text-align: center;
+      font-size: 15px;
+      line-height: 30px;
+    }
+  }
+</style>
+

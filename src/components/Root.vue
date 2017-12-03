a11421067bf7eb282f34b221f6321b897b5df455
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2017-12-03 18:13:16 +0300

    touchable tabs added

--- src/components/Root.vue
+++ src/components/Root.vue
@@ -3,29 +3,39 @@
 
 <template>
   <div class="b-root">
-    <div class="b-root__header">#header</div>
-    <div class="b-root__body">
 
+    <div class="b-root__header">
+      <tbs :siema="siema"/>
+    </div>
+
+    <div class="b-root__body">
       <div class="b-root__sections" ref="sections">
         <div class="b-root__section">Clock</div>
         <div class="b-root__section">Stats</div>
         <div class="b-root__section">Settings</div>
       </div>
-
     </div>
+
   </div>
 </template>
 
 <script>
   import Siema from 'siema';
+  import Tbs from './Tbs';
 
   export default {
     name: 'root',
 
+    components: {
+      Tbs,
+    },
+
+    data: () => ({
+      siema: null,
+    }),
+
     mounted() {
-      this.siema = new Siema({
-        selector: this.$refs.sections,
-      });
+      this.siema = new Siema({ selector: this.$refs.sections });
     },
   };
 </script>
---
+++ src/components/Tbs.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="b-tabs">
+    <li class="b-tabs__item" :class="active(0)" @click="goTo(0)">Timer</li>
+    <li class="b-tabs__item" :class="active(1)" @click="goTo(1)">Stats</li>
+    <li class="b-tabs__item" :class="active(2)" @click="goTo(2)">Settings</li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: 'tbs',
+    props: ['siema'],
+
+    methods: {
+      active(index) {
+        if (this.siema.currentSlide === index) {
+          return { 'b-tabs__item--active': true };
+        }
+
+        return {};
+      },
+
+      goTo(index) {
+        this.siema.goTo(index);
+      },
+    },
+  };
+</script>

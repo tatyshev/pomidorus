2dc82186ebe1710af5cca9a92c14579d4b87c601
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2017-12-17 18:40:08 +0300

    today util added

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,6 +3,15 @@ export const zeroify = (number, fixed = 2) => {
   return (zeros + number).slice(-fixed);
 };
 
+export const today = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+
+  return `${day}:${month}:${year}`;
+};
+
 export const minutes = n => n * 60 * 1000;
 
 export const seconds = n => n * 1000;

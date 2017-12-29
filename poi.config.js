b83c7d5b9e531c874cddd77791139a4135916820
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2017-12-29 17:46:16 +0300

    fix poi homepage

--- poi.config.js
+++ poi.config.js
@@ -6,6 +6,7 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   entry: 'src/index.js',
   sourceMap: isProd ? false : 'eval-source-map',
+  homepage: '',
   karma: {
     frameworks: ['jasmine'],
     browsers: ['jsdom'],

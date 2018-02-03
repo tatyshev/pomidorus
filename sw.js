2a2aa46f28511a18063d64666f8869b9370c9eb9
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-02-03 17:44:28 +0300

    Updates

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-restricted-globals */
 /* eslint-disable no-console */
 
-try {
-  self.addEventListener('notificationclick', (event) => {
-    event.notification.close();
-  });
-} catch (e) {
-  console.error(e);
-}
+/*
+  try {
+    self.addEventListener('notificationclick', (event) => {
+      event.notification.close();
+    });
+  } catch (e) {
+    console.error(e);
+  }
+*/

0436f6727d95fefd0a981c3c7da95f45b257e194
Eduardo San Martin Morote <posva13@gmail.com>
2018-04-15 13:02:15 +0200

    feat: change icon and title when timer is running

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -21,42 +21,65 @@
 </template>
 
 <script>
-  import Focus from '@/lib/index';
-  import Clock from './Clock';
-  import Process from './Process';
-  import Target from './Target';
-  import Heatmap from './Heatmap';
+import Focus from '@/lib/index';
+import {
+  generateTimeIcon,
+  changePageIcon,
+  changePageTitle,
+  resetPageIcon,
+  resetPageTitle,
+  formatTimeForTitle,
+} from '@/lib/utils';
+import Clock from './Clock';
+import Process from './Process';
+import Target from './Target';
+import Heatmap from './Heatmap';
 
-  export default {
-    name: 'timer',
+export default {
+  name: 'timer',
 
-    components: {
-      Process,
-      Target,
-      Clock,
-      Heatmap,
+  components: {
+    Process,
+    Target,
+    Clock,
+    Heatmap,
+  },
+
+  props: {
+    focus: {
+      type: Focus,
+      required: true,
     },
+  },
 
-    props: {
-      focus: {
-        type: Focus,
-        required: true,
-      },
+  data: () => ({
+    stats: [],
+  }),
+
+  mounted() {
+    this.setStats();
+    this.focus.on('update', this.setStats);
+    const reset = () => {
+      resetPageIcon();
+      resetPageTitle();
+    };
+    this.focus.on('finish', reset);
+    this.focus.on('stop', reset);
+    this.focus.on('skip', reset);
+    this.focus.on('tick', () => {
+      const image = generateTimeIcon(
+        this.focus.elapsed / 1000,
+        this.focus.elapsed / this.focus.duration,
+      );
+      changePageIcon(image);
+      changePageTitle(formatTimeForTitle(this.focus.elapsed));
+    });
+  },
+
+  methods: {
+    setStats() {
+      this.stats = JSON.parse(localStorage.getItem('statistics'));
     },
-
-    data: () => ({
-      stats: [],
-    }),
-
-    mounted() {
-      this.setStats();
-      this.focus.on('update', this.setStats);
-    },
-
-    methods: {
-      setStats() {
-        this.stats = JSON.parse(localStorage.getItem('statistics'));
-      },
-    },
-  };
+  },
+};
 </script>
